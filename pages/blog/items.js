3f3a1f20d94414f889b06d78afa4c2fda1cdcd1e
unknown <saint_icarus@protonmail.com>
2022-05-26 21:24:44 +0200

    repost

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -5,6 +5,11 @@ const Items = () =>{
     return(
         <Layout>
             <h1>Items</h1>
+            <ul>
+                <li>Geography</li>
+                <li>Business</li>
+                <li>Mathematics</li>
+            </ul>
         </Layout>
     )
 };
